00b79edb0b9b3e6966b96b4cf806a005324327da
cqq <caiqingqiu@bonc.com.cn>
2019-12-09 19:22:40 +0800

    '申请页面布局'

---
+++ layouts/apply.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="main-container">
+        <header class="apply-header">
+            <div class="nav">
+                <nuxt-link class="dataCloud-logo" to="/"></nuxt-link>
+                <span class="title">数据科学云</span>
+            </div>
+        </header>
+        <div class="main-content">
+            <nuxt class="main-body" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class ApplyPage extends Vue {}
+</script>
+<style lang="less"></style>
+<style lang="less" scoped>
+.apply-header {
+    background: @color-black;
+    height: 56px;
+    padding-left: 16px;
+    line-height: 56px;
+
+    .title {
+        color: @color-FFF;
+        font-size: @fs14;
+    }
+}
+</style>
---
+++ pages/apply.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="apply">
+        申请页面啊
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({
+    layout: 'apply',
+    components: {}
+})
+export default class ApplyPage extends Vue {}
+</script>
+
+<style lang="less" scoped></style>
